3d95f18ae90cf2b68245f7208578512d65381bd2
spectre338 <spectre338@gmail.com>
2022-12-05 09:06:49 -0700

    Move VARS
    Moved VARS that need to be changed for each sensor string into separate file

--- PlatformIO/XMIT_mP_BeeS3_DS18B20/src/main.cpp
+++ PlatformIO/XMIT_mP_BeeS3_DS18B20/src/main.cpp
@@ -4,6 +4,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <BeeS3.h>
+#include <sensorVARS.h>
 
 // Data wire is plugged into port 5 on the Arduino
 #define ONE_WIRE_BUS 10
@@ -11,22 +12,11 @@
 // Setup BeeS3 instance to communicate with Volt Meter
 BEES3 bees3;
 
-// String variable for transmit identification (must be unique)
-String ident = "30:A0";
-
 // Setup a oneWire instance to communicate with OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
  
 // Pass the oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
-
-
-// Addresses of 5 DS18B20 Temp Probes
-uint8_t sensor1[8] = { 0x28, 0x73, 0x53, 0x48, 0xF6, 0x06, 0x3C, 0x13 };
-uint8_t sensor2[8] = { 0x28, 0xBF, 0xF7, 0x48, 0xF6, 0x35, 0x3C, 0xA5 };
-uint8_t sensor3[8] = { 0x28, 0x5B, 0xE2, 0x48, 0xF6, 0x32, 0x3C, 0x90 };
-uint8_t sensor4[8] = { 0x28, 0x45, 0x05, 0x48, 0xF6, 0x37, 0x3C, 0xAC };
-uint8_t sensor5[8] = { 0x28, 0x38, 0x3D, 0x48, 0xF6, 0x9D, 0x3C, 0xCD };
 
 // Variables for battery
 float batt;
@@ -41,9 +31,6 @@ float probe2;
 float probe3;
 float probe4;
 float probe5;
- 
-// Responder MAC Address (Replace with the receiver MAC Address)
-uint8_t broadcastAddress[] = {0x0C, 0xB8, 0x15, 0xc2, 0x51, 0x64};
  
 // Define data structure
 typedef struct struct_message {
@@ -90,7 +77,6 @@ void setup() {
  
   // Define callback
   esp_now_register_send_cb(OnDataSent);
- 
  
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
@@ -178,11 +164,9 @@ void loop() {
   myData.c = probe3;
   myData.d = probe4;
   myData.e = probe5;
-  
   myData.f = ident;
   myData.g = batt;
   myData.h = pac_value;
-  
 
   // Send data
   esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
